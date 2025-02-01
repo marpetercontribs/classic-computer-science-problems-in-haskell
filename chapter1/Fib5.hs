1035878c01f99633027ba19d319b13d0bfe49e37
Markus Peter <marpeter@web.de>
2025-02-01 18:11:17 +0100

    Adjusted beginning comment in source files

--- chapter1/Fib5.hs
+++ chapter1/Fib5.hs
@@ -1,23 +1,19 @@
--- Fib5.hs
--- Adapted From Classic Computer Science Problems in Python/Java Chapter 1
--- Copyright 2025 Markus Peter
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
--- http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
---
--- You wouldn't use a classical "for" loop in Haskell. But something getting
--- close is using a recursion with a counter, which also allows making the
--- recursive algorithm (e.g. of Fib2) tail-recursive, thus faster and much 
--- less memory consuming
+{- Fib5.hs
+   Adapted From Classic Computer Science Problems in Python/Java Chapter 1
+   Copyright 2025 Markus Peter
+  
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+  
+   http://www.apache.org/licenses/LICENSE-2.0
+  
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+-}
 
 module Main where
 
