fb6d161802b91c1168d1d1224d2430d3ce6926f1
Markus Peter <marpeterweb.de>
2026-04-11 21:33:19 +0200

    first version of kmeans with governors example

---
+++ chapter6/Governors.hs
@@ -0,0 +1,98 @@
+{- Governors.hs
+   Adapted From Classic Computer Science Problems in Python/Java Chapter 6
+   Copyright 2026 Markus Peter
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+  
+   http://www.apache.org/licenses/LICENSE-2.0
+  
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+-}
+
+module Main (main) where
+
+import KMeans
+import System.Random(getStdGen)
+
+data Governor = Governor {
+      longitude :: Double
+    , age :: Int
+    , state :: String
+    } deriving (Eq)
+
+instance Clusterable Governor where
+    fromClusterable g = DataPoint {
+          original = g
+        , coordinates = [longitude g, fromIntegral (age g)]
+        , numDimensions = 2
+    }
+
+instance Show Governor where
+    show g = state g ++ " (longitude: " ++ show (longitude g) ++ ", age: " ++ show (age g) ++ ")"
+
+main :: IO ()
+main = do
+    rg <- getStdGen
+    let governors = [
+         Governor (-86.79113::Double)   (72::Int)  "Alabama",
+         Governor (-152.404419::Double) (66::Int)  "Alaska",
+         Governor (-111.431221::Double) (53::Int)  "Arizona",
+         Governor (-92.373123::Double)  (66::Int)  "Arkansas",
+         Governor (-119.681564::Double) (79::Int)  "California",
+         Governor (-105.311104::Double) (65::Int)  "Colorado",
+         Governor (-72.755371::Double)  (61::Int)  "Connecticut",
+         Governor (-75.507141::Double)  (61::Int)  "Delaware",
+         Governor (-81.686783::Double)  (64::Int)  "Florida",
+         Governor (-83.643074::Double)  (74::Int)  "Georgia",
+         Governor (-157.498337::Double) (60::Int)  "Hawaii",
+         Governor (-114.478828::Double) (75::Int)  "Idaho",
+         Governor (-88.986137::Double)  (60::Int)  "Illinois",
+         Governor (-86.258278::Double)  (49::Int)  "Indiana",
+         Governor (-93.210526::Double)  (57::Int)  "Iowa",
+         Governor (-96.726486::Double)  (60::Int)  "Kansas",
+         Governor (-84.670067::Double)  (50::Int)  "Kentucky",
+         Governor (-91.867805::Double)  (50::Int)  "Louisiana",
+         Governor (-69.381927::Double)  (68::Int)  "Maine",
+         Governor (-76.802101::Double)  (61::Int)  "Maryland",
+         Governor (-71.530106::Double)  (60::Int)  "Massachusetts",
+         Governor (-84.536095::Double)  (58::Int)  "Michigan",
+         Governor (-93.900192::Double)  (70::Int)  "Minnesota",
+         Governor (-89.678696::Double)  (62::Int)  "Mississippi",
+         Governor (-92.288368::Double)  (43::Int)  "Missouri",
+         Governor (-110.454353::Double) (51::Int)  "Montana",
+         Governor (-98.268082::Double)  (52::Int)  "Nebraska",
+         Governor (-117.055374::Double) (53::Int)  "Nevada",
+         Governor (-71.563896::Double)  (42::Int)  "New Hampshire",
+         Governor (-74.521011::Double)  (54::Int)  "New Jersey",
+         Governor (-106.248482::Double) (57::Int)  "New Mexico",
+         Governor (-74.948051::Double)  (59::Int)  "New York",
+         Governor (-79.806419::Double)  (60::Int)  "North Carolina",
+         Governor (-99.784012::Double)  (60::Int)  "North Dakota",
+         Governor (-82.764915::Double)  (65::Int)  "Ohio",
+         Governor (-96.928917::Double)  (62::Int)  "Oklahoma",
+         Governor (-122.070938::Double) (56::Int)  "Oregon",
+         Governor (-77.209755::Double)  (68::Int)  "Pennsylvania",
+         Governor (-71.51178::Double)   (46::Int)  "Rhode Island",
+         Governor (-80.945007::Double)  (70::Int)  "South Carolina",
+         Governor (-99.438828::Double)  (64::Int)  "South Dakota",
+         Governor (-86.692345::Double)  (58::Int)  "Tennessee",
+         Governor (-97.563461::Double)  (59::Int)  "Texas",
+         Governor (-111.862434::Double) (70::Int)  "Utah",
+         Governor (-72.710686::Double)  (58::Int)  "Vermont",
+         Governor (-78.169968::Double)  (60::Int)  "Virginia",
+         Governor (-121.490494::Double) (66::Int)  "Washington",
+         Governor (-80.954453::Double)  (66::Int)  "West Virginia",
+         Governor (-89.616508::Double)  (49::Int)  "Wisconsin",
+         Governor (-107.30249::Double)  (55::Int)  "Wyoming"]
+    let km = newKMeans 2 governors rg
+    let clusters = runKMeans km 100
+    mapM_ (\(cluster, idx) -> do
+        putStrLn $ "Cluster " ++ show idx ++ ":"
+        mapM_ (putStrLn . ("  " ++) . show . original) (dataPoints cluster)) (zip clusters [0..])
+    putStrLn "Done"
---
+++ chapter6/lib/KMeans.hs
@@ -0,0 +1,163 @@
+{- KMeans.hs
+   Adapted From Classic Computer Science Problems in Python/Java Chapter 6
+   Copyright 2026 Markus Peter
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+  
+   http://www.apache.org/licenses/LICENSE-2.0
+  
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+-}
+
+{-# LANGUAGE FlexibleInstances #-}
+
+module KMeans(
+      DataPoint(..)
+    , Clusterable(..)
+    , Cluster(..)
+    , KMeans(..)
+    , newKMeans
+    , runKMeans
+    , zscores
+) where
+
+import Data.List(transpose)
+import System.Random(RandomGen, randomR)
+import Debug.Trace(trace)
+
+-- corresponds to the "Into<DataPoint>" trait in my Rust implementation
+class (Eq a) => Clusterable a where
+    fromClusterable :: a -> DataPoint a
+
+-- DataPoint can be a data structure like in my Rust implementation, so that it can hold data
+data (Clusterable p) => DataPoint p = DataPoint {
+      original :: p
+    , coordinates :: [Double]
+    , numDimensions :: Int
+}
+
+instance Clusterable [Double] where
+    fromClusterable x = DataPoint {
+          original = x
+        , coordinates = x
+        , numDimensions = length x
+    }
+
+data (Clusterable p) => Cluster p = Cluster {
+      dataPoints :: [DataPoint p]
+    , centroid :: DataPoint [Double]
+}
+
+data (Clusterable p) => KMeans p = KMeans {
+      points :: [DataPoint p]
+    , clusters :: [Cluster p]
+    }
+
+distance :: (Clusterable p, Clusterable q) => DataPoint p -> DataPoint q -> Double
+distance this that = sqrt . sum $ zipWith (\x y -> (x - y) ^ 2) (coordinates this) (coordinates that)
+
+newKMeans :: (Clusterable p, RandomGen g) => Int -> [p] -> g -> KMeans p
+newKMeans k ps rg = let dps = zScoreNormalize ps in
+     KMeans {
+          points = dps
+        , clusters = fst $ foldl (\(cs,rg') _ ->
+            let (c,rg'') = randomPoint dps rg'
+            in (Cluster { dataPoints = [], centroid = c }:cs, rg'')) ([],rg) [1..k]
+     }
+
+zScoreNormalize :: (Clusterable p) => [p] -> [DataPoint p]
+zScoreNormalize ps = zipWith (\dp zsd -> dp { coordinates = zsd }) dataPoints zScoredDimensions
+    where dataPoints = map fromClusterable ps
+          dimensions = case dataPoints of
+                            [] -> 0 -- handle empty list case
+                            (dp:_) -> numDimensions dp
+          zScoredDimensions = transpose $
+            map (\dim -> zscores (dimensionSlice dim dataPoints)) [0..dimensions - 1]
+
+randomPoint :: (Clusterable p, RandomGen g) => [DataPoint p] -> g -> (DataPoint [Double], g)
+randomPoint dps rg = case dps of
+    [] -> error "Cannot select a random point from an empty list"
+    (dp:_) ->
+        let (cs, rg''') = foldl (\(coords,rg') dim ->
+                let values = dimensionSlice dim dps
+                    (c,rg'') = randomR (minimum values, maximum values) rg'
+                in (coords ++ [c], rg''))
+                ([],rg) [0..numDimensions dp - 1]
+        in (fromClusterable cs, rg''')
+
+runKMeans :: (Clusterable p) => KMeans p -> Int -> [Cluster p]
+runKMeans kmeans maxIterations
+    | maxIterations <= 0 = clusters kmeans
+    | otherwise =   let nextClusters = (generateCentroids . assignClusters) kmeans
+                    in  if coordinatesAreEqual (map centroid nextClusters) (map centroid (clusters kmeans))
+                        -- Ausgabe stimmt noch nicht weil rückwärts gezählt wird!
+                        then trace ("Converged after " ++ (show maxIterations) ++ " iterations") $ clusters kmeans
+                        else runKMeans kmeans { clusters = nextClusters } (maxIterations - 1)
+
+assignClusters :: (Clusterable p) => KMeans p -> [Cluster p]
+assignClusters kmeans = foldl (\cs dp -> addToClosestCluster dp cs)
+    (map (\c-> c { dataPoints = [] }) (clusters kmeans)) -- the clusters should be cleared before points are assigned
+    (points kmeans)
+
+maxDouble :: Double
+maxDouble = x
+  where n = floatDigits x
+        b = floatRadix x
+        (_, u) = floatRange x
+        x = encodeFloat (b^n - 1) (u - n)
+
+closestCluster :: (Clusterable p) => DataPoint p -> [Cluster p] -> Int
+closestCluster dp clusters = case clusters of
+    [] -> error "No clusters available to assign the data point"
+    (c1:_) -> snd $ foldl (\(lowestDistance,closestClusterIdx) (clstr, idx) ->
+            let centroidDistance = distance dp (centroid clstr)
+            in  if centroidDistance < lowestDistance
+                then (centroidDistance, idx)
+                else (lowestDistance, closestClusterIdx))
+        (maxDouble,0) (zip clusters [0..])
+
+addToClosestCluster :: (Clusterable p) => DataPoint p -> [Cluster p] -> [Cluster p]
+addToClosestCluster dp clusters = let closestIdx = closestCluster dp clusters
+    in map (\(c,idx) -> if idx == closestIdx then c { dataPoints = dp : dataPoints c } else c)
+        (zip clusters [0..])
+-- alternative implementation using take, ++ and drop:    
+        -- take closestIdx clusters ++
+        --  [ (clusters !! closestIdx) { dataPoints = dp : dataPoints (clusters !! closestIdx) } ] ++
+        --  drop (closestIdx + 1) clusters
+
+generateCentroids :: (Clusterable p) => [Cluster p] -> [Cluster p]
+generateCentroids clusters = map (\cluster -> case dataPoints cluster of
+        [] -> cluster -- handle empty cluster case
+        dps@(dp:_) ->
+            let dimensions = numDimensions dp
+                newCentroid = fromClusterable $ 
+                    map (\dim -> mean (dimensionSlice dim dps)) [0..dimensions - 1]
+            in cluster { centroid = newCentroid })
+    clusters
+
+coordinatesAreEqual :: [DataPoint [Double]] -> [DataPoint [Double]] -> Bool
+coordinatesAreEqual dps1 dps2 = all (\(dp1, dp2) -> coordinates dp1 == coordinates dp2)
+    (zip dps1 dps2)
+
+dimensionSlice :: (Clusterable p) => Int -> [DataPoint p] -> [Double]
+dimensionSlice dim dataPoints = map (\dp -> coordinates dp !! dim) dataPoints
+
+zscores :: [Double] -> [Double]
+zscores xs = let m = mean xs
+                 s = stdev xs
+             in if s == 0
+                then replicate (length xs) 0
+                else map (\x -> (x - m) / s) xs
+
+mean :: [Double] -> Double
+mean xs = sum xs / fromIntegral (length xs)
+
+stdev :: [Double] -> Double
+stdev xs = sqrt (sum (map (\x -> (x - m) ^ 2) xs) / fromIntegral (length xs))
+    where m = mean xs
